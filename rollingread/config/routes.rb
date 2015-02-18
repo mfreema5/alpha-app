711ee20f28ae282b7c9f5764a09b23f0b13d5ca5
Michael Freeman <mfreema5@hawk.iit.edu>
2015-02-17 19:23:06 -0600

    Add 'haml-rails\ bundle.  Rebuild scaffolds.

--- rollingread/config/routes.rb
+++ rollingread/config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  resources :courses
+
+  resources :sources
+
+  resources :sections
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
