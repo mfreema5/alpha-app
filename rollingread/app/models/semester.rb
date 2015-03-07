ed84854896be8b55c60f35803e0fe29b79265876
Mike Freeman <mfreema5@hawk.iit.edu>
2015-03-07 17:28:45 -0600

    Comment out some features as part of trouble-shooting. Removed the "uniqueness" validation from semesters.rb Reduced the fixtures loaded by test_helper.rb.

--- rollingread/app/models/semester.rb
+++ rollingread/app/models/semester.rb
@@ -1,5 +1,5 @@
 class Semester < ActiveRecord::Base
   has_many :courses
   validates :short_title, :start_date, :end_date, presence: true
-  validates :short_title, uniqueness: true
+  # validates :short_title, uniqueness: true
 end
