e416acb44f3a901ccc471713d88c64c4e4975fcd
Mike Freeman <mfreema5@hawk.iit.edu>
2015-03-08 14:06:42 -0500

    Updated views to use associations. Add "through:" associations to models "semester", "course" and "sources".

--- rollingread/app/models/course.rb
+++ rollingread/app/models/course.rb
@@ -1,6 +1,7 @@
 class Course < ActiveRecord::Base
   belongs_to :semester
   has_many :sources
+  has_many :assignments, through: :sources
 
   validates :short_title, :meet_days, presence: true
   # validates :short_title, uniqueness: true
--- rollingread/app/models/semester.rb
+++ rollingread/app/models/semester.rb
@@ -1,5 +1,7 @@
 class Semester < ActiveRecord::Base
   has_many :courses
+  has_many :sources, through: :courses
+  has_many :assignments, through: :sources
   validates :short_title, :start_date, :end_date, presence: true
   # validates :short_title, uniqueness: true
 end
