70f99a0a8ec9eb439a2fc97429959d324e35d01e
Mike Freeman <mfreema5@hawk.iit.edu>
2015-03-13 07:47:30 -0500

    Add validations to models. Add a background color to "create new" links on index pages.

--- rollingread/app/models/semester.rb
+++ rollingread/app/models/semester.rb
@@ -1,6 +1,11 @@
 class Semester < ActiveRecord::Base
   has_many :courses
 
-  # validates :short_semester, :start_date, :end_date, presence: true
+  validates :short_semester, :start_date, :end_date, presence: true
 
+
+#t.string   "short_semester"
+#t.text     "full_semester"
+#t.date     "start_date"
+#t.date     "end_date"
 end
--- rollingread/app/models/source.rb
+++ rollingread/app/models/source.rb
@@ -2,6 +2,6 @@ class Source < ActiveRecord::Base
   belongs_to :course, inverse_of: :sources
   has_many :assignments
 
-  # validates :short_source, presence: true
+  validates :short_source, :course_id, presence: true
 
 end
